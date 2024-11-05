d6ee2dc4541614b2c9967baa12e088efa2c91773
Apu Kumar <apukumar180@gmail.com>
2024-11-05 22:07:07 +0600

    add: dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Personal_Portfolio/', // Replace with your actual repository name
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 })
