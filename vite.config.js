dca6b222bf2a129becd1c347883a0fd9d81d17fb
Apu Kumar <apukumar180@gmail.com>
2024-11-05 22:53:39 +0600

    add:deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal_Portfolio/', // Replace with your actual repository name
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+  base: '/Personal_Portfolio/' // Ensure this matches your repo name
 })
+
 
